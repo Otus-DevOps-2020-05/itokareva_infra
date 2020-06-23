21a1fa5588ceeabf48f9879adacaf4eb448f9533
itokareva <itokareva@yandex.ru>
2020-06-23 18:02:59 +0300

    add scripts for deploy reddit

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # itokareva_infra
 itokareva Infra repository
+#Домашняя работа 5
 bastion_IP = 130.193.50.9
 someinternalhost_IP = 10.130.0.15
-18.06.2020 11 BM
+# Домашняя работа 6
+testapp_IP = 84.201.175.66
+testapp_port = 9292
+# Самостоятельная работа
+# install_ruby.sh install_mongodb.sh deploy.sh  
+# Дополнительное задание
+# create_ycvm.sh metadata.yaml http://84.201.159.22:9292/
  
---
+++ install_mongodb.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+if [ $? -eq 0 ]
+then 
+sudo systemctl start mongod
+sudo systemctl enable mongod
+sudo systemctl status mongod
+else
+echo "Failure: mongodb is not installed"
+exit 1
+fi
+
